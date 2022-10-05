fd09c296be2c19e0e801c3af98e3aba969d070e7
daffamanaf <112881086+daffamanaf@users.noreply.github.com>
2022-10-05 23:10:45 +0700

    Tugas6 Daffa Adil Manaf_Gunadarma

---
+++ navbar.php
@@ -0,0 +1,16 @@
+<h1><a href="index.html">Daffa Adil Manaf</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
+      <h2>I'm a passionate <span>Web Programming</span> from Indonesia</h2>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="#header">Home</a></li>
+          <li><a class="nav-link" href="#about">About</a></li>
+          <li><a class="nav-link" href="#resume">Resume</a></li>
+          <!--li><a class="nav-link" href="#services">Services</a></li-->
+          <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
+          <!--li><a class="nav-link" href="#contact">Contact</a></li-->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
---
+++ social-links.php
@@ -0,0 +1,6 @@
+<div class="social-links">
+        <a href="#" target="_blank" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="https://www.facebook.com/daffa.adymanaf" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/daffamnf_/?hl=id" target="_blank" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" target="_blank" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      </div>
